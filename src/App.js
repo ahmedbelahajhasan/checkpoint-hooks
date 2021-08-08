3daa7757660b147dea2190c448a28f8c3066570e
ahmed belhaj hasan <ahmed.belhajhasan@esprit.tn>
2021-08-08 10:23:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {useState} from "react"
+import { Data } from './components/Data';
+import MoviesList from './components/moviesList/MoviesList';
+import AddMovie from './components/add/AddMovie';
+import Header from './components/header/Header';
+
+function App() {
+  const [movies, setMovies] = useState(Data)
+  const handleMovie = (newMovie) => {
+      setMovies([...movies, newMovie])
+  }
+  
+  return (
+    <div className="App">
+      <Header />
+      <MoviesList movies={movies}/>
+      <AddMovie handleMovie={handleMovie}/>
+    </div>
+  );
+}
+
+export default App;
+
